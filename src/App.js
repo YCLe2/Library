c9afc7d1754c0d3bc0f087a930893dea10aadd9d
YCLe2 <chany4430@gmail.com>
2024-12-03 10:24:57 +0900

    final_final

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css'; // 스타일 파일 가져오기
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchPage from "./components/SearchPage";
 import ResultsPage from "./components/ResultsPage";
-import DetailedSearchPage from "./components/DetailedSearchPage";
 import Header from "./components/header";
 
 const App = () => {
@@ -15,7 +14,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<SearchPage />} />
           <Route path="/results" element={<ResultsPage />} />
-          <Route path="/detailed-search" element={<DetailedSearchPage />} />
         </Routes>
       </Router>
     </>
--- src/components/header.js
+++ src/components/header.js
@@ -10,6 +10,11 @@ const Header = () => {
                     <img src="https://i.ibb.co/TbTrTJ0/logo.png" alt="한동대학교" className="logo-img" />
                 </a>
             </div>
+            <div className="logoRight">
+                <a href="https://www.nl.go.kr/">
+                    <img src="https://i.namu.wiki/i/u1G8Qe7pzvfK6CBzVYh0F5l5JpNj1iRHc022mjib1Ecmhki2ahDBfSwdf1mQhUhJtbXXM9c9R8kCDgrakXPNlka7yOcTPvEpEVpYHN7BGmBqFo80h5dPLR8sxIuPXDy14_ucdjTDfGuGGExfrO0T0A.svg" alt="국립중앙도서관" className="logo-right" />
+                </a>
+            </div>
         </div>
         <div className="divTopMenu">
             <div className="topMenu">
@@ -17,9 +22,6 @@ const Header = () => {
                     <ul>
                         <li class="menu1">
                             <a href="/results">검색</a>
-                        </li>
-                        <li class="menu2">
-                            <a href="/detailed-search">상세검색</a>
                         </li>
                         <li class="menu3">
                             <a href="https://hisnet.handong.edu/">Hisnet</a>
